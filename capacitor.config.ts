33cf965a401a3294c2bfe86d8af6813fe3199ad7
Palwai Tejaswini <palwaitejaswini7777@gmail.com>
2026-06-16 14:52:50 +0530

    setup capacitor for ios and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.beathealth',
+  appName: 'BEAT',
+  webDir: 'dist'
+};
+
+export default config;
